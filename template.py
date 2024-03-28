64f98d2f28d603af18c5febfdab96a14b9c362a2
Deepak Thakur <dt9971678995@gmail.com>
2024-03-28 11:15:42 +0530

    requirements.txt updated

--- template.py
+++ template.py
@@ -21,7 +21,7 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
 
     if filedir !="":
-        os.makedirs(filedir, exist_ok=True)
+        os.makedirs(filedir, exist_ok=True) 
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
