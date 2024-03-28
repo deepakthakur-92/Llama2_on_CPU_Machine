26236f5c77c15831dc7fd894d8a5c7c9cfc8cb7b
Deepak Thakur <dt9971678995@gmail.com>
2024-03-28 11:29:17 +0530

    updated setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = "Deepak Thakur",
+    author_email='deepak2009thakur@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
